954c13e67a4b1b3991e66565d5b86f475c90cb9a
Guilherme <guilhermenepomuceno46@gmail.com>
2019-06-04 16:01:55 -0300

    Adicionando configuraçoes docker e atualizando dependencias do composer

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . "/vendor/autoload.php";
-
+date_default_timezone_set("America/Fortaleza");
 // Instantiate the app
 $settings = require __DIR__ . '/src/settings.php';
 $app = new \Slim\App($settings);
