c6e666753e3e3b3760ce76da2a1852c83c55f28d
Guilherme <guilhermenepomuceno46@gmail.com>
2019-06-06 15:03:10 -0300

    scroll to id

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -7,8 +7,16 @@ $(document).ready(function(){
         }
     });
 
-    $('a[href="#top"]').click(function(){
-        $('html, body').animate({scrollTop : 0},800);
-        return false;
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var targetEle = this.hash;
+        var $targetEle = $(targetEle);
+
+        $('html, body').stop().animate({
+            'scrollTop': $targetEle.offset().top
+        }, 800, 'swing', function () {
+            window.location.hash = targetEle;
+        });
     });
 });
