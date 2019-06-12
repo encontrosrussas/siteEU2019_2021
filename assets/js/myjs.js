cb8855ff5b7f7ffdf3a435209a7df7b0433acf46
Guilherme Nepomuceno <guilhermenepomuceno46@gmail.com>
2019-06-12 09:47:23 -0300

    Correção da estrutura do html responsividade no carrosel

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,49 +1,55 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('a[href="#top"]').fadeIn();
-        } else {
-            $('a[href="#top"]').fadeOut();
-        }
-    });
-
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-
-        var targetEle = this.hash;
-        var $targetEle = $(targetEle);
-
-        $('html, body').stop().animate({
-            'scrollTop': $targetEle.offset().top
-        }, 800, 'swing', function () {
-            window.location.hash = targetEle;
-        });
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+        $('a[href="#top"]').fadeIn();
+    } else {
+        $('a[href="#top"]').fadeOut();
+    }
+});
+$('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var targetEle = this.hash;
+    var $targetEle = $(targetEle);
+    $('html, body').stop().animate({
+        'scrollTop': $targetEle.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = targetEle;
     });
 });
-
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-       nav:true // mostra os botões
-    })
- });
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+$('.carousel').carousel({
+    interval: 4000
+});
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false,
+            loop: true,
+            autoplayHoverPause: true,
         },
-        600:{
-            items:3
+        480: {
+            items: 2,
+            nav: false,
+            loop: true,
+            autoplayHoverPause: true,
         },
-        1000:{
-            items:5
+        600: {
+            items: 3,
+            nav: false,
+            loop: true,
+            autoplayHoverPause: true,
+        },
+        1000: {
+            items: 4,
+            nav: false,
+            loop: true,
+            autoplayHoverPause: true,
         }
     }
-})
-
-$('#blogCarousel').carousel({
-    interval: 5000
 });
+
+$("#ano").html(new Date().getFullYear());
