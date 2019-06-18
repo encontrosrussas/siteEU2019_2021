e93431a8c7470eb257b16843b2f6f6875ec23c5c
Your Name <you@example.com>
2019-06-18 16:44:27 -0300

    Botões do Carrosel e container timeline

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -18,38 +18,51 @@ $('a[href^="#"]').on('click', function (e) {
 $('.carousel').carousel({
     interval: 4000
 });
-$(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 15,
-    autoplay: true,
-    autoplayTimeout: 2500,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false,
-            loop: true,
-            autoplayHoverPause: true,
-        },
-        480: {
-            items: 2,
-            nav: false,
-            loop: true,
-            autoplayHoverPause: true,
-        },
-        600: {
-            items: 3,
-            nav: false,
-            loop: true,
-            autoplayHoverPause: true,
-        },
-        1000: {
-            items: 4,
-            nav: false,
-            loop: true,
-            autoplayHoverPause: true,
+$(document).ready(function ($){
+    var carrosel = $(".owl-carousel");
+    carrosel.owlCarousel({
+        loop: true,
+        margin: 15,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+                loop: true,
+                autoplayHoverPause: true,
+            },
+            480: {
+                items: 2,
+                nav: false,
+                loop: true,
+                autoplayHoverPause: true,
+            },
+            600: {
+                items: 3,
+                nav: false,
+                loop: true,
+                autoplayHoverPause: true,
+            },
+            1000: {
+                items: 4,
+                nav: false,
+                loop: true,
+                autoplayHoverPause: true,
+            }
         }
-    }
+    });
+    var carroselData = carrosel.data()['owl.carousel'];
+    var tmTgt = $('.go');
+    tmTgt.on('click', function(e){
+        e.preventDefault();
+        if ($(this).hasClass('go-left')){
+            carroselData.prev();
+        } else {
+            carroselData.next();
+        }
+    });
 });
 
 $("#ano").html(new Date().getFullYear());
