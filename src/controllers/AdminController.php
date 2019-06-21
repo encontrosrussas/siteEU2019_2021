b9cf0170e115b9ea0b34ced67e18c7ba72a543a6
Guilherme Nepomuceno <guilhermenepomuceno46@gmail.com>
2019-06-21 11:50:06 -0300

    dashboard e template

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -14,13 +14,19 @@ class AdminController
 
     public function login($request, $response, $args)
     {
-        $this->container->get('logger')->info("slim-app '/' route");
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
         return $this->container->view->render($response, 'admin/login.html');
+    }
+
+    public function dashboard($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/index.html');
     }
 
     public function template($request, $response, $args)
     {
-        $this->container->get('logger')->info("slim-app '/' route");
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
         return $this->container->view->render($response, 'admin/template/'.$args['nome']);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,7 @@ use app\controllers\AdminController;
 return function ($app) {
     $app->group("/admin", function ($app) {
         $app->get('/login', AdminController::class . ':login')->setName('login-admin');
+        $app->get('/dashboard', AdminController::class . ':dashboard')->setName('dashboard-admin');
         $app->get('/temp/{nome}', AdminController::class . ':template')->setName('template');
     });
     $app->get('/{nome}', HomeController::class . ':home')->setName('index');
