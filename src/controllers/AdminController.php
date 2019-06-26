d8c91344e0955b77b7bdd192f09337e73a8b9c1a
Guilherme Nepomuceno <guilhermenepomuceno46@gmail.com>
2019-06-26 09:56:40 -0300

    Corrigindo Problemas na Pagina de Login Criação do Layout da Dashboard Criação das rotas

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -24,6 +24,48 @@ class AdminController
         return $this->container->view->render($response, 'admin/index.html');
     }
 
+    public function usuarios($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/usuarios.html');
+    }
+
+    public function editais($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/editais.html');
+    }
+
+    public function paginas($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/paginas.html');
+    }
+
+    public function cronogramas($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/cronogramas.html');
+    }
+
+    public function apresentacoes($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/apresentacoes.html');
+    }
+
+    public function mini_cursos($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/mini_cursos.html');
+    }
+
+    public function palestras($request, $response, $args)
+    {
+        $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
+        return $this->container->view->render($response, 'admin/palestras.html');
+    }
+
     public function template($request, $response, $args)
     {
         $this->container->get('logger')->info("slim-app '{$_SERVER['REQUEST_URI']}' route");
--- src/routes.php
+++ src/routes.php
@@ -7,6 +7,13 @@ return function ($app) {
     $app->group("/admin", function ($app) {
         $app->get('/login', AdminController::class . ':login')->setName('login-admin');
         $app->get('/dashboard', AdminController::class . ':dashboard')->setName('dashboard-admin');
+        $app->get('/usuarios', AdminController::class . ':usuarios')->setName('usuarios-admin');
+        $app->get('/editais', AdminController::class . ':editais')->setName('editais-admin');
+        $app->get('/paginas', AdminController::class . ':paginas')->setName('paginas-admin');
+        $app->get('/cronogramas', AdminController::class . ':cronogramas')->setName('cronogramas-admin');
+        $app->get('/apresentacoes', AdminController::class . ':apresentacoes')->setName('apresentacoes-admin');
+        $app->get('/mini_cursos', AdminController::class . ':mini_cursos')->setName('mini_cursos-admin');
+        $app->get('/palestras', AdminController::class . ':palestras')->setName('palestras-admin');
         $app->get('/temp/{nome}', AdminController::class . ':template')->setName('template');
     });
     $app->get('/{nome}', HomeController::class . ':home')->setName('index');
