60c388ff5532a26e3e3dd867580b3d3857085a7f
Guilherme <guilhermenepomuceno46@gmail.com>
2019-08-23 08:52:31 -0300

    Merge Backend

---
+++ src/settings.php
@@ -0,0 +1,21 @@
+<?php
+// Altere as Configurações e renomeie para settings.php
+return [
+    'settings' => [
+        'debug' => true,
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+        'logger' => [
+            'name' => 'slim-app',
+            'level' => Monolog\Logger::DEBUG,
+            'path' => __DIR__ .DIRECTORY_SEPARATOR.'..'. DIRECTORY_SEPARATOR . 'logs'. DIRECTORY_SEPARATOR .'app.log',
+        ],
+        'db'=>[
+            'type' => 'mysql',
+            'host' => 'db',
+            'user' => 'user',
+            'pass' => 'password',
+            'dbname' => 'EU',
+            'port' => 3306
+        ]
+]];
