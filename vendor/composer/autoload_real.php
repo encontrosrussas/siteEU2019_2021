9e38cf6ebed8c09fb84c3b4e6dfeb060053c3b54
susana moreira <suzanasuzy12@hotmail.com>
2019-09-13 09:47:32 -0300

    não lembro dessas alterações

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit65e3524db8bad57e97df3b8d3fb51e87
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit65e3524db8bad57e97df3b8d3fb51e87', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
